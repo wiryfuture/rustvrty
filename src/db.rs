865cfe7df01aa71e54b4a46e55e7c134987c5f9c
Philip Wilk <wiryfuture@gmail.com>
2023-01-21 14:12:00 +0000

    add migration

--- src/db.rs
+++ src/db.rs
@@ -1,16 +1,16 @@
 use sea_orm::{ConnectOptions, Database, DatabaseConnection};
 use std::env;
 
-async fn connect() -> Result<DatabaseConnection, &str> {
+async fn connect() -> Result<DatabaseConnection, &'static str> {
     match env::var("VRTY_DB_CONN_STRING") {
         Ok(conn) => {
-            let mut opt = ConnectOptions::new(conn);
+            let opt = ConnectOptions::new(conn);
             let connection = Database::connect(opt).await;
             match connection {
                 Ok(c) => Ok(c),
                 Err(_) => Err("Failed connection to db"),
             }
         }
-        Err(e) => Err(()),
+        Err(_) => Err("No db connection string found - add it to envvar VRTY_DB_CONN_STRING"),
     }
 }
