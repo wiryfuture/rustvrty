c499e59ee7f035f83687b4c70fe99c959f099efb
Philip Wilk <wiryfuture@gmail.com>
2023-02-02 20:18:44 +0000

    entities?

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,8 @@
 mod sec;
 mod db;
 pub use crate::sec::{login, User};
+
+use migration::{Migrator, MigratorTrait};
+
+let connection = sea_orm::Database::connect(&database_url).await?;
+Migrator::up(&connection, None).await?;
